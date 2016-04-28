f3447bd2633db386935a84192a3f62fd4efcabb2
Mat Scales <mscales@google.com>
2016-04-28 13:44:58 +0100

    Add the TTL header, now required by the spec

--- webpush/push_test.go
+++ webpush/push_test.go
@@ -42,6 +42,10 @@ func TestSendWebPush(t *testing.T) {
 			t.Errorf("Expected body to be length %d, was %d", expectedLength, len(body))
 		}
 
+		if request.Header.Get("TTL") == "" {
+			t.Error("Expected TTL header to be set")
+		}
+
 		if request.Header.Get("Content-Encoding") != "aesgcm" {
 			t.Errorf("Expected Content-Encoding header to be aesgcm, got %v", request.Header.Get("Content-Encoding"))
 		}
